d2c645a6a8ad3ce8e3c1e8c7d1c9fd916d4cd709
MikhailCh <mike_ch@mail.ru>
2019-04-30 01:51:16 +0700

    add ties

--- src/main/scala/fintech/domain.scala
+++ src/main/scala/fintech/domain.scala
@@ -8,8 +8,10 @@ import cats.implicits._
 @JsonCodec
 final case class Group(id: UUID, name: String, users: List[User]) {
   def addUser(name: String): Group = {
+    val newUserID = UUID.randomUUID()
+    val newUser = User(newUserID, name, users.map(user => Tie(user.id, 0.0)))
     println(s"Adding user with name $name") // todo delete
-    copy(users = User(UUID.randomUUID(), name, List()) :: users)
+    copy(users = newUser :: users.map(user => User(user.id, user.name, Tie(newUserID, 0.0) :: user.ties)))
   }
 
   def removeUser(id: UUID): Group = copy(users = users.filter(user => user.id != id))
@@ -23,9 +25,19 @@ object Group {
 }
 
 @JsonCodec
-final case class User(id: UUID, name: String, ties: List[Double])
+final case class User(id: UUID, name: String, ties: List[Tie])
 object User {
   implicit val key: Key[User] = new Key[User] {
+    def key(a: User): UUID = a.id
+    def notFound(uuid: UUID): Throwable = UserNotFound(uuid)
+    def multiple(uuid: UUID): Throwable = MultipleUsers(uuid)
+  }
+}
+
+@JsonCodec
+final case class Tie(idTo: UUID, value: Double)
+object Tie {
+  implicit val key: Key[User] = new Key[User] { // todo Change exception
     def key(a: User): UUID = a.id
     def notFound(uuid: UUID): Throwable = UserNotFound(uuid)
     def multiple(uuid: UUID): Throwable = MultipleUsers(uuid)
